a2cfe30a2de86623b2900833c9f57def0d5ecb46
Jeremy Siek <jsiek@129-79-241-203.dhcp-bl.indiana.edu>
2017-07-11 17:15:58 -0400

    blah

--- LangDesignImplVerif/IntArithVars.thy
+++ LangDesignImplVerif/IntArithVars.thy
@@ -1,7 +1,7 @@
 section "A Language of Integer Arithmetic and Variables"
 
 theory IntArithVars
-  imports Main
+  imports Main "~~/src/HOL/Library/FSet"
 begin
   
 type_synonym name = nat
@@ -28,6 +28,32 @@ fun E :: "exp \<Rightarrow> env \<Rightarrow> input \<Rightarrow> (int \<times> 
   "E (ELet x e1 e2) \<rho> stdin =
      (case E e1 \<rho> stdin of None \<Rightarrow> None
       | Some (n,stdin') \<Rightarrow> E e2 (\<rho>(x\<mapsto>n)) stdin')"
+
+fun FV :: "exp \<Rightarrow> name fset" where
+  "FV (EInt n) = {||}" |
+  "FV (ERead) = {||}" |
+  "FV (ENeg e) = FV e" |
+  "FV (EAdd e1 e2) = FV e1 |\<union>| FV e2" |
+  "FV (EVar x) = {|x|}" |
+  "FV (ELet x e1 e2) = FV e1 |\<union>| (FV e2 - {|x|})" 
+
+fun max_var :: "name fset \<Rightarrow> name" where
+  "max_var s = ffold max 0 s"
+  
+interpretation max: comp_fun_commute "max::nat\<Rightarrow>nat\<Rightarrow>nat"
+  unfolding comp_fun_commute_def by auto
+    
+lemma max_greater_equal: fixes x::nat shows "x \<in> fset s \<Longrightarrow> x \<le> ffold max 0 s"
+  apply (induction s arbitrary: x)
+  apply force apply simp apply (erule disjE) apply force apply force done
+
+lemma fresh_var: "Suc (ffold max 0 s) \<notin> fset s"
+proof
+  assume 1: "Suc (ffold max 0 s) \<in> fset s"
+  let ?m = "ffold max 0 s"
+  from 1 have "Suc ?m \<le> ?m" using max_greater_equal by blast
+  thus "False" by simp
+qed
 
 section "Intermediate Language in A-normal form"
   
@@ -71,23 +97,67 @@ fun Ss :: "stmt list \<Rightarrow> state_xfr" where
   "Ss [] = ret" |
   "Ss (s#ss) = seq (S s) (Ss ss)"
 
+fun Fs :: "fexp \<Rightarrow> state \<Rightarrow> (int \<times> input) option" where
+  "Fs e (\<rho>,i) = (case F e \<rho> of None \<Rightarrow> None | Some n \<Rightarrow> Some (n,i))"
+
 fun B :: "block \<Rightarrow> state \<Rightarrow> (int \<times> input) option" where
-  "B (ss,e) = seq (Ss ss) (\<lambda>(\<rho>,i). case F e \<rho> of None \<Rightarrow> None | Some n \<Rightarrow> Some (n,i))"
+  "B (ss,e) = seq (Ss ss) (Fs e)"
+
+fun FVa :: "atom \<Rightarrow> name fset" where
+  "FVa (AInt n) = {||}" |
+  "FVa (AVar x) = {|x|}" 
+  
+fun FVf :: "fexp \<Rightarrow> name fset" where
+  "FVf (Atom a) = FVa a" |
+  "FVf (FNeg a) = FVa a" |
+  "FVf (FAdd a1 a2) = FVa a1 |\<union>| FVa a2"
+    
+fun FVss :: "stmt list \<Rightarrow> name fset \<Rightarrow> name fset" where
+  "FVss [] s = s" |
+  "FVss ((Assign x e)#ss) s = FVf e |\<union>| (FVss ss s - {|x|})" |
+  "FVss ((Read x)#ss) s = FVss ss s - {|x|}"
+  
+fun WVs :: "stmt list \<Rightarrow> name fset" where
+  "WVs [] = {||}" |
+  "WVs ((Assign x e)#ss) = finsert x (WVs ss)" |
+  "WVs ((Read x)#ss) = finsert x (WVs ss)"
 
 lemma seq_ret[simp]: "seq ret f = f"
   unfolding seq_def ret_def apply (rule ext) apply auto done
 
-lemma seq_assoc[simp]: "seq (seq f1 f2) f3 = seq f1 (seq f2 f3)" 
+lemma seq_assoc: "seq (seq f1 f2) f3 = seq f1 (seq f2 f3)" 
   unfolding seq_def apply (rule ext) apply (case_tac "f1 \<sigma>") apply auto done
 
 lemma Ss_append_seq[simp]: "Ss (ss1@ss2) = seq (Ss ss1) (Ss ss2)"
-  by (induction ss1) auto
+  by (induction ss1) (auto simp: seq_assoc)
     
 lemma seq1_none[simp]: "f1 \<sigma> = None \<Longrightarrow> seq f1 f2 \<sigma> = None"
   by (simp add: seq_def)
     
 lemma seq1_some[simp]: "f1 \<sigma> = Some \<sigma>' \<Longrightarrow> seq f1 f2 \<sigma> = f2 \<sigma>'"
   by (simp add: seq_def)  
+
+lemma A_weakening: "\<forall> x \<in> fset (FVa a).\<rho> x = \<rho>' x \<Longrightarrow> A a \<rho> = A a \<rho>'"
+  by (induction a arbitrary: \<rho> \<rho>') auto
+    
+lemma F_weakening: "\<lbrakk> \<forall> x \<in> fset (FVf e).\<rho> x = \<rho>' x \<rbrakk> \<Longrightarrow> F e \<rho> = F e \<rho>'"
+  apply (induction e arbitrary: \<rho> \<rho>')
+  using A_weakening apply force
+  using A_weakening apply force
+  apply (subgoal_tac "\<forall>x\<in>fset (FVa x1a). \<rho> x = \<rho>' x") prefer 2 apply force
+  apply (subgoal_tac "\<forall>x\<in>fset (FVa x2a). \<rho> x = \<rho>' x") prefer 2 apply force
+  apply (subgoal_tac "A x1a \<rho> = A x1a \<rho>'") prefer 2 using A_weakening apply force
+  apply (subgoal_tac "A x2a \<rho> = A x2a \<rho>'") prefer 2 using A_weakening apply force
+  apply simp apply (case_tac "A x1a \<rho>") apply auto
+    apply (case_tac "A x1a \<rho>'") apply auto
+  done  
+
+lemma Ss_weakening: "\<lbrakk> \<forall> x \<in> fset (FVss ss {||}). \<rho> x = \<rho>' x \<rbrakk> \<Longrightarrow> Ss ss (\<rho>,i) = Ss ss (\<rho>',i)"
+  apply (induction ss arbitrary: \<rho> \<rho>' i)
+  apply simp
+  sorry    
+    
+(* need a way to compose disjoint computations *)
     
 section "Compilation Pass: Flattening"
 
@@ -121,11 +191,13 @@ value "flatten_program (ENeg (EAdd (EInt 1) (ENeg (EInt 2))))"
   
 section "Correctness of Flattening"
 
-lemma atomize_correct: "atomize e k = (k', ss, a) \<Longrightarrow> B ([], e) = B (ss, Atom a)"
+
+  
+lemma atomize_correct: "\<lbrakk> atomize e k = (k', ss, a) \<rbrakk> \<Longrightarrow> Fs e = seq (Ss ss) (Fs (Atom a))"
   apply (cases e)
   apply force
   apply simp apply clarify apply (rule ext) apply (case_tac x) apply simp
-    apply (case_tac "A x2 aa") apply auto
+    apply (case_tac "A x2 aa") apply (auto simp: seq_assoc)
   apply (rule ext) apply (case_tac x) apply (simp add: seq_def) 
     apply (case_tac "A x31 aa") apply (auto simp: seq_def)
     apply (case_tac "A x32 aa") apply (auto simp: seq_def)
@@ -140,8 +212,8 @@ next
   case (Some \<sigma>)
   from Some obtain \<rho>' i' where Ss_ss1: "Ss ss1 (\<rho>,i) = Some (\<rho>',i')" by (cases \<sigma>) auto
   from 1 have A: "B (ss2, e1) (\<rho>',i') = B (ss2', e2) (\<rho>',i')" by simp
-  from Ss_ss1 A show ?thesis by simp
-qed  
+  from Ss_ss1 A show ?thesis by (simp add: seq_assoc)
+qed
     
 lemma flatten_correct: "flatten e k = (k', ss, e') \<Longrightarrow> E e \<rho> i = B (ss, e') (\<rho>, i)"
 proof (induction e arbitrary: k k' e' \<rho> i ss)
@@ -153,7 +225,8 @@ proof (induction e arbitrary: k k' e' \<rho> i ss)
 next
   case ERead
   have 2: "flatten ERead k = (Suc k, [Read k], Atom (AVar k))" by simp
-  have 3: "E ERead \<rho> i = B ([Read k], Atom (AVar k)) (\<rho>, i)" by (cases i) (auto simp: seq_def) 
+  have 3: "E ERead \<rho> i = B ([Read k], Atom (AVar k)) (\<rho>, i)"
+    by (cases i) (auto simp: seq_def seq_assoc) 
   from ERead 2 3 show ?case by simp
 next
   case (ENeg e k k' e' \<rho> i ss)
@@ -165,16 +238,9 @@ next
   from 1 have 2: "B (ss1, e1) (\<rho>, i) = B (ss1 @ ss2, Atom a) (\<rho>, i)"
     using B_append[of "[]" e1 ss2 "Atom a"] by simp
   from IH 2 have 3: "E e \<rho> i = B (ss1 @ ss2, Atom a) (\<rho>, i)" by simp
-  have 4: "E (ENeg e) \<rho> i = B (ss1 @ ss2, FNeg a) (\<rho>, i)"
-  proof (cases "E e \<rho> i")
-    case None
-    from this 3 show ?thesis apply (case_tac "seq (Ss ss1) (Ss ss2) (\<rho>,i)") apply force
-      apply (case_tac aa) apply (case_tac "A a aaa") apply auto done
-  next
-    case (Some \<sigma>')
-    from this 3 show ?thesis  apply (case_tac "seq (Ss ss1) (Ss ss2) (\<rho>,i)") apply force apply auto
-      apply (case_tac "A a aa") apply auto done
-  qed
+  from 3 have 4: "E (ENeg e) \<rho> i = B (ss1 @ ss2, FNeg a) (\<rho>, i)"
+    apply (cases "seq (Ss ss1) (Ss ss2) (\<rho>,i)") apply auto
+    apply (case_tac "A a aa") apply auto done
   from 4 fne ENeg show ?case by auto      
 next
   case (EAdd e1 e2)

bb424344632e4d05f28ff14468a667e0d09508d0
Jeremy Siek <jsiek@indiana.edu>
2017-07-11 23:00:39 -0400

    blah

--- LangDesignImplVerif/IntArithVars.thy
+++ LangDesignImplVerif/IntArithVars.thy
@@ -4,88 +4,66 @@ theory IntArithVars
   imports Main "~~/src/HOL/Library/FSet"
 begin
   
-type_synonym name = nat
+type_synonym index = nat
  
-datatype exp = EInt int | ERead | ENeg exp | EAdd exp exp | EVar name | ELet name exp exp
+datatype exp = EInt int | ERead | EPrim "int\<Rightarrow>int\<Rightarrow>int" exp exp | EVar index | ELet exp exp
 
 type_synonym input = "int list"
 
-type_synonym env = "name \<rightharpoonup> int"
+type_synonym val = int
+type_synonym env = "val list"
 
-fun E :: "exp \<Rightarrow> env \<Rightarrow> input \<Rightarrow> (int \<times> input) option" where
-  "E (EInt n) \<rho> stdin = Some (n,stdin)" |
-  "E (ERead) \<rho> stdin = (case stdin of [] \<Rightarrow> None
-                        | Cons n stdin' \<Rightarrow> Some (n, stdin'))" |
-  "E (ENeg e) \<rho> stdin = (case E e \<rho> stdin of None \<Rightarrow> None
-                        | Some (n,stdin') \<Rightarrow> Some (- n, stdin'))" |
-  "E (EAdd e1 e2) \<rho> stdin =
-     (case E e1 \<rho> stdin of None \<Rightarrow> None
-      | Some (n1,stdin') \<Rightarrow> 
-        (case E e2 \<rho> stdin' of
+fun E :: "exp \<Rightarrow> env \<Rightarrow> input \<Rightarrow> (val \<times> input) option" where
+  "E (EInt n) \<rho> i = Some (n,i)" |
+  "E (ERead) \<rho> i = (case i of [] \<Rightarrow> None
+                        | Cons n i' \<Rightarrow> Some (n, i'))" |
+  "E (EPrim f e1 e2) \<rho> i =
+     (case E e1 \<rho> i of None \<Rightarrow> None
+      | Some (n1,i') \<Rightarrow> 
+        (case E e2 \<rho> i' of
           None \<Rightarrow> None
-        | Some (n2, stdin'') \<Rightarrow> Some (n1 + n2, stdin'')))" |
-  "E (EVar x) \<rho> stdin = (case \<rho> x of None \<Rightarrow> None | Some n \<Rightarrow> Some (n, stdin))" |
-  "E (ELet x e1 e2) \<rho> stdin =
-     (case E e1 \<rho> stdin of None \<Rightarrow> None
-      | Some (n,stdin') \<Rightarrow> E e2 (\<rho>(x\<mapsto>n)) stdin')"
+        | Some (n2, i'') \<Rightarrow> Some (f n1 n2, i'')))" |
+  "E (EVar n) \<rho> i = (if n < length \<rho> then Some (\<rho>!n,i) else None)" |
+  "E (ELet e1 e2) \<rho> i =
+     (case E e1 \<rho> i of None \<Rightarrow> None
+      | Some (n,i') \<Rightarrow> E e2 (n#\<rho>) i')"
 
-fun FV :: "exp \<Rightarrow> name fset" where
-  "FV (EInt n) = {||}" |
-  "FV (ERead) = {||}" |
-  "FV (ENeg e) = FV e" |
-  "FV (EAdd e1 e2) = FV e1 |\<union>| FV e2" |
-  "FV (EVar x) = {|x|}" |
-  "FV (ELet x e1 e2) = FV e1 |\<union>| (FV e2 - {|x|})" 
-
-fun max_var :: "name fset \<Rightarrow> name" where
-  "max_var s = ffold max 0 s"
-  
-interpretation max: comp_fun_commute "max::nat\<Rightarrow>nat\<Rightarrow>nat"
-  unfolding comp_fun_commute_def by auto
-    
-lemma max_greater_equal: fixes x::nat shows "x \<in> fset s \<Longrightarrow> x \<le> ffold max 0 s"
-  apply (induction s arbitrary: x)
-  apply force apply simp apply (erule disjE) apply force apply force done
-
-lemma fresh_var: "Suc (ffold max 0 s) \<notin> fset s"
-proof
-  assume 1: "Suc (ffold max 0 s) \<in> fset s"
-  let ?m = "ffold max 0 s"
-  from 1 have "Suc ?m \<le> ?m" using max_greater_equal by blast
-  thus "False" by simp
-qed
+fun shifte :: "nat \<Rightarrow> nat \<Rightarrow> exp \<Rightarrow> exp" where
+  "shifte k c (EInt n) = (EInt n)" |
+  "shifte k c (ERead) = (ERead)" |
+  "shifte k c (EPrim f e1 e2) = EPrim f (shifte k c e1) (shifte k c e2)" |
+  "shifte k c (EVar n) = (if c \<le> n then EVar (n + k) else EVar n)" |
+  "shifte k c (ELet e1 e2) = ELet (shifte k c e1) (shifte k (Suc c) e2)"
 
 section "Intermediate Language in A-normal form"
   
-datatype atom = AInt int | AVar name
-datatype fexp = Atom atom | FNeg atom | FAdd atom atom
-datatype stmt = Assign name fexp | Read name 
-type_synonym block = "stmt list \<times> fexp"
+datatype atom = AInt int | AVar index
+datatype fexp = Atom atom | FPrim "int\<Rightarrow>int\<Rightarrow>int" atom atom
+datatype stmt = Push fexp | Read 
+type_synonym block = "stmt list \<times> atom"
 
 fun A :: "atom \<Rightarrow> env \<Rightarrow> int option" where
   "A (AInt n) \<rho> = Some n" |
-  "A (AVar x) \<rho> = \<rho> x" 
-  
+  "A (AVar k) \<rho> = (if k < length \<rho> then Some (\<rho>!k) else None)" 
+
 fun F :: "fexp \<Rightarrow> env \<Rightarrow> int option" where
   "F (Atom a) \<rho> = A a \<rho>" |
-  "F (FNeg e) \<rho> = (case A e \<rho>  of None \<Rightarrow> None
-                        | Some n \<Rightarrow> Some (- n))" |
-  "F (FAdd e1 e2) \<rho> =
+  "F (FPrim f e1 e2) \<rho> =
      (case A e1 \<rho> of None \<Rightarrow> None
       | Some n1 \<Rightarrow> 
         (case A e2 \<rho> of
           None \<Rightarrow> None
-        | Some n2 \<Rightarrow> Some (n1 + n2)))"
+        | Some n2 \<Rightarrow> Some (f n1 n2)))"
 
 type_synonym state = "env \<times> input"
 type_synonym state_xfr = "state \<Rightarrow> state option"
 
 fun S :: "stmt \<Rightarrow> state_xfr" where
-  "S (Assign x e) (\<rho>, i) =
+  "S (Push e) (\<rho>, i) =
      (case F e \<rho> of None \<Rightarrow> None
-      | Some n \<Rightarrow> Some (\<rho>(x\<mapsto>n), i))" |
-  "S (Read x) (\<rho>, i) = (case i of [] \<Rightarrow> None
-                     | Cons n i' \<Rightarrow> Some (\<rho>(x\<mapsto>n), i'))"
+      | Some n \<Rightarrow> Some (n#\<rho>, i))" |
+  "S (Read) (\<rho>, i) = (case i of [] \<Rightarrow> None
+                     | Cons n i' \<Rightarrow> Some (n#\<rho>, i'))"
   
 definition seq :: "('a \<Rightarrow> 'b option) \<Rightarrow> ('b \<Rightarrow> 'c option) \<Rightarrow> 'a \<Rightarrow> 'c option" where
   "seq f1 f2 \<sigma> \<equiv> (case f1 \<sigma> of None \<Rightarrow> None | Some \<sigma>' \<Rightarrow> f2 \<sigma>')"
@@ -97,30 +75,27 @@ fun Ss :: "stmt list \<Rightarrow> state_xfr" where
   "Ss [] = ret" |
   "Ss (s#ss) = seq (S s) (Ss ss)"
 
-fun Fs :: "fexp \<Rightarrow> state \<Rightarrow> (int \<times> input) option" where
-  "Fs e (\<rho>,i) = (case F e \<rho> of None \<Rightarrow> None | Some n \<Rightarrow> Some (n,i))"
+fun As :: "atom \<Rightarrow> state \<Rightarrow> (int \<times> input) option" where
+  "As e (\<rho>,i) = (case A e \<rho> of None \<Rightarrow> None | Some n \<Rightarrow> Some (n,i))"
 
 fun B :: "block \<Rightarrow> state \<Rightarrow> (int \<times> input) option" where
-  "B (ss,e) = seq (Ss ss) (Fs e)"
+  "B (ss,e) = seq (Ss ss) (As e)"
 
-fun FVa :: "atom \<Rightarrow> name fset" where
-  "FVa (AInt n) = {||}" |
-  "FVa (AVar x) = {|x|}" 
-  
-fun FVf :: "fexp \<Rightarrow> name fset" where
-  "FVf (Atom a) = FVa a" |
-  "FVf (FNeg a) = FVa a" |
-  "FVf (FAdd a1 a2) = FVa a1 |\<union>| FVa a2"
-    
-fun FVss :: "stmt list \<Rightarrow> name fset \<Rightarrow> name fset" where
-  "FVss [] s = s" |
-  "FVss ((Assign x e)#ss) s = FVf e |\<union>| (FVss ss s - {|x|})" |
-  "FVss ((Read x)#ss) s = FVss ss s - {|x|}"
-  
-fun WVs :: "stmt list \<Rightarrow> name fset" where
-  "WVs [] = {||}" |
-  "WVs ((Assign x e)#ss) = finsert x (WVs ss)" |
-  "WVs ((Read x)#ss) = finsert x (WVs ss)"
+fun shifta :: "nat \<Rightarrow> nat \<Rightarrow> atom \<Rightarrow> atom" where
+  "shifta k c (AInt n) = (AInt n)" |
+  "shifta k c (AVar n) = (if c \<le> n then AVar (n + k) else AVar n)" 
+
+fun shiftf :: "nat \<Rightarrow> nat \<Rightarrow> fexp \<Rightarrow> fexp" where
+  "shiftf k c (Atom a) = Atom (shifta k c a)" |
+  "shiftf k c (FPrim f e1 e2) = FPrim f (shifta k c e1) (shifta k c e2)"
+
+fun shifts :: "nat \<Rightarrow> nat \<Rightarrow> stmt \<Rightarrow> stmt" where
+  "shifts k c (Read) = Read" |
+  "shifts k c (Push e) = Push (shiftf k c e)"
+
+fun shiftss :: "nat \<Rightarrow> nat \<Rightarrow> stmt list \<Rightarrow> stmt list" where
+  "shiftss k c [] = []" |
+  "shiftss k c (s#ss) = (shifts k c s)#(shiftss k (Suc c) ss)"
 
 lemma seq_ret[simp]: "seq ret f = f"
   unfolding seq_def ret_def apply (rule ext) apply auto done
@@ -137,61 +112,48 @@ lemma seq1_none[simp]: "f1 \<sigma> = None \<Longrightarrow> seq f1 f2 \<sigma> 
 lemma seq1_some[simp]: "f1 \<sigma> = Some \<sigma>' \<Longrightarrow> seq f1 f2 \<sigma> = f2 \<sigma>'"
   by (simp add: seq_def)  
 
-lemma A_weakening: "\<forall> x \<in> fset (FVa a).\<rho> x = \<rho>' x \<Longrightarrow> A a \<rho> = A a \<rho>'"
-  by (induction a arbitrary: \<rho> \<rho>') auto
-    
-lemma F_weakening: "\<lbrakk> \<forall> x \<in> fset (FVf e).\<rho> x = \<rho>' x \<rbrakk> \<Longrightarrow> F e \<rho> = F e \<rho>'"
-  apply (induction e arbitrary: \<rho> \<rho>')
-  using A_weakening apply force
-  using A_weakening apply force
-  apply (subgoal_tac "\<forall>x\<in>fset (FVa x1a). \<rho> x = \<rho>' x") prefer 2 apply force
-  apply (subgoal_tac "\<forall>x\<in>fset (FVa x2a). \<rho> x = \<rho>' x") prefer 2 apply force
-  apply (subgoal_tac "A x1a \<rho> = A x1a \<rho>'") prefer 2 using A_weakening apply force
-  apply (subgoal_tac "A x2a \<rho> = A x2a \<rho>'") prefer 2 using A_weakening apply force
-  apply simp apply (case_tac "A x1a \<rho>") apply auto
-    apply (case_tac "A x1a \<rho>'") apply auto
-  done  
-
-lemma Ss_weakening: "\<lbrakk> \<forall> x \<in> fset (FVss ss {||}). \<rho> x = \<rho>' x \<rbrakk> \<Longrightarrow> Ss ss (\<rho>,i) = Ss ss (\<rho>',i)"
-  apply (induction ss arbitrary: \<rho> \<rho>' i)
-  apply simp
-  sorry    
-    
-(* need a way to compose disjoint computations *)
-    
 section "Compilation Pass: Flattening"
 
-fun atomize :: "fexp \<Rightarrow> nat \<Rightarrow> nat \<times> (stmt list) \<times> atom" where
-  "atomize (Atom a) k = (k, [], a)" |
-  "atomize f k = (Suc k, [Assign k f], AVar k)"
-
-fun flatten :: "exp \<Rightarrow> nat \<Rightarrow> nat \<times> block" where
-  "flatten (EInt n) k = (k, [], Atom (AInt n))" |
-  "flatten (ERead) k = (k+1, [Read k], Atom (AVar k))" |
-  "flatten (ENeg e) k = 
-    (let (k1,ss1,e') = flatten e k in
-     let (k2,ss2,a) = atomize e' k1 in
-     (k2, ss1@ ss2, FNeg a))" |
-  "flatten (EAdd e1 e2) k =
-    (let (k1,ss1,e1') = flatten e1 k in
-     let (k2,ss2,e2') = flatten e2 k1 in
-     let (k3,ss3,a1) = atomize e1' k2 in
-     let (k4,ss4,a2) = atomize e2' k3 in
-     (k4, ss1 @ ss2 @ ss3 @ ss4, FAdd a1 a2))" |
-  "flatten (EVar x) k = (k, [], Atom (AVar x))" |
-  "flatten (ELet x e1 e2) k = 
-    (let (k1,ss1,e1') = flatten e1 k in
-     let (k2,ss2,e2') = flatten e2 k1 in
-     (k2, ss1 @ (Assign x e1') # ss2, e2'))"
+fun flatten :: "exp \<Rightarrow> block" where
+  "flatten (EInt n) = ([], AInt n)" |
+  "flatten (ERead) = ([Read], AVar 0)" |
+  "flatten (EPrim f e1 e2) =
+    (let (ss1,a1) = flatten e1 in
+     let (ss2,a2) = flatten e2 in
+     let a1' = shifta (length ss2) 0 a1 in
+     (ss1 @ ss2 @ [Push (FPrim f a1' a2)], AVar 0))" |
+  "flatten (EVar x) = ([], AVar x)" |
+  "flatten (ELet e1 e2) = 
+    (let (ss1,a1) = flatten e1 in
+     let (ss2,a2) = flatten e2 in
+     (ss1 @ (Push (Atom a1)) # ss2, a2))"
 
 fun flatten_program :: "exp \<Rightarrow> block" where
-  "flatten_program e = (let (k,ss,e') = flatten e 0 in (ss, e'))" 
+  "flatten_program e = (let (ss,e') = flatten e in (ss, e'))" 
 
-value "flatten_program (ENeg (EAdd (EInt 1) (ENeg (EInt 2))))"
+fun eadd :: "exp \<Rightarrow> exp \<Rightarrow> exp" where
+  "eadd e1 e2 = EPrim (\<lambda>a.\<lambda>b. a+b) e1 e2"
+fun esub :: "exp \<Rightarrow> exp \<Rightarrow> exp" where
+  "esub e1 e2 = EPrim (\<lambda>a.\<lambda>b. a-b) e1 e2"
+
+section "A Little Testing"
+  
+(*  (4 + 40) - (7 - 5) = 42*)
+definition P0 :: exp where
+  "P0 \<equiv> esub (eadd (EInt 4) (EInt 40)) (esub (EInt 7) (EInt 5))"
+
+value "E P0 [] []"
+value "flatten_program P0"
+value "B (flatten_program P0) ([],[])"
+
+definition P1 :: exp where
+  "P1 \<equiv> ELet P0 (eadd (esub (EInt 2) (EInt 2)) (EVar 0))"
+
+value "E P1 [] []" 
+value "flatten_program P1"
+value "B (flatten_program P1) ([],[])"   
   
 section "Correctness of Flattening"
-
-
   
 lemma atomize_correct: "\<lbrakk> atomize e k = (k', ss, a) \<rbrakk> \<Longrightarrow> Fs e = seq (Ss ss) (Fs (Atom a))"
   apply (cases e)
